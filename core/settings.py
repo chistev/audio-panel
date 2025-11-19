b4dedb73d5cfd5436e8e5e809ec033ab73012d77
chistev <stephenowabie@gmail.com>
2025-11-19 22:43:40 +0100

    registration page

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'panel'
+    'panel',
+    'accounts',
 ]
 
 MIDDLEWARE = [
