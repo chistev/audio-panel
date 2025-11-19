5ef32f57e7e80db094b21b2388c1c91eca0d0703
chistev <stephenowabie@gmail.com>
2025-11-19 22:58:54 +0100

    terms of service page

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('terms/', views.terms, name='terms'),
+    path('privacy/', views.privacy, name='privacy'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render, redirect
 
 def register(request):
     return render(request, 'accounts/register.html')
+
+def terms(request):
+    return render(request, 'accounts/terms.html')
+
+def privacy(request):
+    return render(request, 'accounts/privacy.html')
