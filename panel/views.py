37b27e82a033303f53302f02fd8ef70f72bf96e5
chistev <stephenowabie@gmail.com>
2025-11-20 11:41:06 +0100

    dashboard

--- panel/views.py
+++ panel/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def landing(request):
     return render(request, 'panel/landing.html')
+
+def dashboard(request):
+    return render(request, 'panel/dashboard.html')
